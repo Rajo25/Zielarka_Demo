1a361f21385e2adff52d4a194f691201532ec69f
Rajo25 <krysztalowysmok24@gmail.com>
2026-01-17 00:57:50 +0100

    MovementBaza
    działający bazowy movement (Na razie bez skoku)

--- Zielarka_Demo/Assets/Scripts/Player/PlayerController.cs
+++ Zielarka_Demo/Assets/Scripts/Player/PlayerController.cs
@@ -10,17 +10,29 @@ public class PlayerController : MonoBehaviour
     public float velPower = 10f;
     private float _moveInput;
     public float frictionAmount = 0.2f;
+    private bool _isGrounded = true;
         
     private void FixedUpdate()
     {
+        Movement();
+    }
 
-        float targetSpeed = _moveInput * moveSpeed;
+    private void Movement()
+    {
+        float targetSpeed = _moveInput * moveSpeed * 100 * Time.deltaTime;
         float speedDiff = targetSpeed - rb.linearVelocity.x;
         float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : deceleration;
         float movement = Mathf.Pow(Mathf.Abs(speedDiff) * accelRate, velPower) * Mathf.Sign(speedDiff);
 
-        rb.AddForce(movement * Vector2.right);
+        rb.AddForce(Vector2.right * (movement * Time.deltaTime));
         
+        //adds artificial friction
+        if (_isGrounded && Mathf.Abs(_moveInput) < 0.01f)
+        {
+            float amount = Mathf.Min(Mathf.Abs(rb.linearVelocity.x), Mathf.Abs(frictionAmount));
+            amount *=Mathf.Sign(rb.linearVelocity.x);
+            rb.AddForce(Vector2.right * -amount,  ForceMode2D.Impulse);
+        }
     }
 
     void Update()
