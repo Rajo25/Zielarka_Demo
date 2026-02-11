58a7cdb775ccf54f549cb745ad721b3bf33a3ea1
Rajo25 <krysztalowysmok24@gmail.com>
2026-02-11 16:10:12 +0100

    zapisuje jeśli coś by się zepsuło abym miał do czego wrócić

---
+++ Zielarka_Demo/Assets/Scripts/Enemy/Enemy_01Controller.cs
@@ -0,0 +1,8 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Enemy_01Controller : MonoBehaviour
+{
+    public float damage = 10f;
+    public float knockbackForce = 10f;
+}
--- Zielarka_Demo/Assets/Scripts/IDamageable.cs
+++ Zielarka_Demo/Assets/Scripts/IDamageable.cs
@@ -1,11 +1,10 @@
 using UnityEngine;
 
-public interface IDamagable
+public interface IDamageable
 {
     public float Health { set; get; }
     public bool Targetable {  set; get; }
     public bool Invincible { set; get; }
-    public void OnHit(int damage, Vector2 knockback);
-    public void OnHit(int damage);
-    public void OnDeath();
+    public void OnHit(float damage, Vector2 knockback);
+    public void OnHit(float damage);
 }
