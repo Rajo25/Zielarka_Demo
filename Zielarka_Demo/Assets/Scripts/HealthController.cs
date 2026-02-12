72782d81912a7aebc100c666d78086bc0a9acdfb
Rajo25 <krysztalowysmok24@gmail.com>
2026-02-12 02:40:59 +0100

    HealthControllerDziala i wrogowie mogą zadawać dmg

--- Zielarka_Demo/Assets/Scripts/HealthController.cs
+++ Zielarka_Demo/Assets/Scripts/HealthController.cs
@@ -1,9 +1,12 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HealthController : MonoBehaviour
 {
     public int health = 10;
     [SerializeField] private int currentHealth;
+    private bool _isDamaged;
+    public LayerMask whatIsEnemy;
 
     void Start()
     {
@@ -12,12 +15,28 @@ public class HealthController : MonoBehaviour
 
     void Update()
     {
+        
         if (currentHealth <= 0)
         {
             Die();
         }
     }
 
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        _isDamaged =  true;
+    }
+
+    void OnCollisionEnter2D(Collision2D whatIsEnemy)
+    {
+        AttackController damage = whatIsEnemy.gameObject.GetComponent<AttackController>();
+        if (damage != null)
+        { 
+            TakeDamage(damage.attackDamage);
+            _isDamaged = false;
+        }
+    }
     void Die()
     {
         print("died");
